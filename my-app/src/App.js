d3c315ff2268155b4e218a172eebd357591b6b38
Safiyeshka <koroglu1.s.i.22@gmail.com>
2024-02-20 09:33:35 +0300

    1-11

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,23 +2,47 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( <>
+  	//7
+	 <div>
+		<p class= 'a1'>text</p>
+		<p class= 'a2'>text</p>
+		<p class= 'a3'>text</p>
+	</div>;
+	
+	//8
+	<ul>
+	<li>text1</li>
+	<li>text2</li>
+	<li>text3</li>
+	<li>text4</li>
+	<li>text5</li>
+	<li>text6</li>
+	<li>text7</li>
+	<li>text8</li>
+	<li>text9</li>
+	<li>text10</li>
+	</ul>
+
+	//9
+	<><ul>
+		<li>text1</li>
+		<li>text2</li>
+		<li>text3</li>
+	</ul>
+	<ul>
+		<li>text1</li>
+		<li>text2</li>
+		<li>text3</li>
+	</ul>;
+	</>
+
+	//11
+	<input />
+
+
+  </>
+
   );
 }
 
