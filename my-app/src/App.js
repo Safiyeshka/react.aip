37164d6d1d8ad09e1abaefaecd51fbca919012b2
Safiyeshka <koroglu1.s.i.22@gmail.com>
2024-03-05 16:52:06 +0300

    23-33

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -328,6 +328,235 @@ import './App.css';
 		);
 	  }
 
+	  //34
+
+	  function App() {
+		function showAlert(number) {
+		  alert(number);
+		}
+	  
+		return (
+		  <div>
+			<button onClick={() => showAlert(1)}>act1</button>
+			<button onClick={() => showAlert(2)}>act2</button>
+			<button onClick={() => showAlert(3)}>act3</button>
+		  </div>
+		);
+	  }
+
+	  //35.1
+	  function App() {
+		function handleClick(event) {
+		  console.log(event);
+		}
+	  
+		return (
+		  <div>
+			<button onClick={handleClick}>Click me</button>
+		  </div>
+		);
+	  }
+	  
+
+	  //35.2
+	  function App() {
+		function handleClick(event) {
+		  console.log(event.target);
+		}
+	  
+		return (
+		  <div>
+			<button onClick={handleClick}>Click me</button>
+		  </div>
+		);
+	  }
+
+	  //36.2
+	  function App() {
+		function func(arg1, arg2) {
+		  console.log(arg1, arg2);
+		}
+	  
+		return (
+		  <div>
+			<button onClick={() => func('eee', 'event')}>act</button>
+		  </div>
+		);
+	  }
+
+	  //36.3
+	  function App() {
+		function func(event, arg) {
+		  console.log(event, arg);
+		}
+	  
+		return (
+		  <div>
+			<button onClick={event => func(event, 'eee')}>act</button>
+		  </div>
+		);
+	  }
+
+	  //36.4
+	  function App() {
+		function func(arg1, event, arg2) {
+		  console.log(arg1, event, arg2);
+		}
+	  
+		return (
+		  <div>
+			<button onClick={event => func('arg1', event, 'arg2')}>act</button>
+		  </div>
+		);
+	  }
+
+	  //37
+	function App() {
+	const arr = [
+		<li>1</li>,
+		<li>2</li>,
+		<li>3</li>,
+		<li>4</li>,
+		<li>5</li>,
+	];
+
+	return (
+		<ul>
+		{arr}
+		</ul>
+	);
+	}
+
+	//38
+	function App() {
+		const arr = [];
+	
+		for (let i = 1; i <= 5; i++) {
+		arr.push(<li>{i}</li>);
+		}
+	
+		return (
+		<ul>
+			{arr}
+		</ul>
+		);
+	}
+	
+	//39
+	function App() {
+		const arr = ['a', 'b', 'c', 'd', 'e'];
+	  
+		const res = arr.map(function(item) {
+		  return <li>{item}</li>;
+		});
+	  
+		return (
+		  <ul>
+			{res}
+		  </ul>
+		);
+	  }
+
+	//40
+	function App() {
+		const arr = [1, 2, 3, 4, 5];
+	  
+		const res = arr.map(function(item, index) {
+		  return <p key={index}>{item}</p>;
+		});
+	  
+		return (
+		  <div>
+			{res}
+		  </div>
+		);
+	  }
+
+	//41
+	function App() {
+		const users = [
+		  { name: 'user1', surn: 'surn1', age: 30 },
+		  { name: 'user2', surn: 'surn2', age: 31 },
+		  { name: 'user3', surn: 'surn3', age: 32 },
+		];
+	  
+		const res = users.map(function(item, index) {
+		  return (
+			<li key={index}>
+			  <span>{item.name}</span>:
+			  <span>{item.surn}</span>,
+			  <span>{item.age}</span>
+			</li>
+		  );
+		});
+	  
+		return (
+		  <ul>
+			{res}
+		  </ul>
+		);
+	  }
+
+	  //42
+		
+	function App() {
+		const prods = [
+		{ id: 1, name: 'product1', cost: 10 },
+		{ id: 2, name: 'product2', cost: 20 },
+		{ id: 3, name: 'product3', cost: 30 },
+		];
+	
+		const res = prods.map(function(item) {
+		return (
+			<p key={item.id}>
+			<span>{item.name}</span>:
+			<span>{item.cost}</span>
+			</p>
+		);
+		});
+	
+		return (
+		<div>
+			{res}
+		</div>
+		);
+	}
+
+	//43
+	function App() {
+		const users = [
+		  { id: 1, name: 'user1', surn: 'surn1', age: 30 },
+		  { id: 2, name: 'user2', surn: 'surn2', age: 31 },
+		  { id: 3, name: 'user3', surn: 'surn3', age: 32 },
+		];
+	  
+		const rows = users.map(function(item) {
+		  return (
+			<tr key={item.id}>
+			  <td>{item.name}</td>
+			  <td>{item.surn}</td>
+			  <td>{item.age}</td>
+			</tr>
+		  );
+		});
+	  
+		return (
+		  <table>
+			<thead>
+			  <tr>
+				<th>Имя</th>
+				<th>Фамилия</th>
+				<th>Возраст</th>
+			  </tr>
+			</thead>
+			<tbody>
+			  {rows}
+			</tbody>
+		  </table>
+		);
+	  }
+
+	
 
 
 
